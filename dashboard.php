478296d23c49ced3d97b05395f0580259a84430a
Alan Rodriguez <alanrodriguez.nuevas@gmail.com>
2019-10-17 15:05:15 -0300

    Estadistica Google Api

--- dashboard.php
+++ dashboard.php
@@ -8,6 +8,7 @@
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="src/css/estatico-adm.css">
     <link rel="stylesheet" href="src/css/dashboard.css">
+
 </head>
 <body>
     
@@ -62,8 +63,107 @@
                 </div>
             </div>            
         </section>
+        <section class="api-google">
+            <div id="chart_div"></div>
+        </section>
     </main>
     <script src="src/js/jquery.js"></script>
     <script src="src/js/menu.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script>
+        var aux = $(window).width();
+        $(window).resize(function(){
+            aux = $(window).width();
+            drawChart();
+        });
+    </script>
+    
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+        
+        google.charts.load('current', {'packages':['line', 'corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+
+        var button = document.getElementById('change-chart');
+        var chartDiv = document.getElementById('chart_div');
+
+        var data = new google.visualization.DataTable();
+        data.addColumn('date', 'Mes');
+        data.addColumn('number', "Tickets");
+        data.addColumn('number', "Pendientes");
+
+        data.addRows([
+        [new Date(2019, 0),  10,  0],
+        [new Date(2019, 1),   20, 10],
+        [new Date(2019, 2),   0, 0],
+        [new Date(2019, 3),  15, 15],
+        [new Date(2019, 4),  40, 18],
+        [new Date(2019, 5),    10, 0],
+        [new Date(2019, 6), 5, 2],
+        [new Date(2019, 7), 20, 16],
+        [new Date(2019, 8),  40, 13],
+        [new Date(2019, 9),  10,  9],
+        [new Date(2019, 10), 0,  0],
+        [new Date(2019, 11), 30,  5]
+        ]);
+
+        var materialOptions = {
+        chart: {
+        title: 'Estadistica Tickets Año 2019'
+        },
+        width: aux,
+        height: 250,
+        series: {
+        // Gives each series an axis name that matches the Y-axis below.
+        0: {axis: 'Temps'},
+        1: {axis: 'Daylight'}
+        },
+        axes: {
+        // Adds labels to each axis; they don't have to match the axis names.
+        y: {
+        Tickets: {label: 'Nº'},
+        Pendientes: {label: 'Nº'}
+        }
+        }
+        };
+
+        var classicOptions = {
+        title: 'Estadistica de Tickets 2019',
+        width: aux,
+        height: 250,
+        // Gives each series an axis that matches the vAxes number below.
+        series: {
+        0: {targetAxisIndex: 0},
+        1: {targetAxisIndex: 1}
+        },
+        vAxes: {
+        // Adds titles to each axis.
+        0: {title: 'Nº Tickets'},
+        1: {title: 'Nº Pendientes'}
+        },
+        hAxis: {
+        ticks: [new Date(2019, 0), new Date(2019, 1), new Date(2019, 2), new Date(2019, 3),
+                new Date(2019, 4),  new Date(2019, 5), new Date(2019, 6), new Date(2019, 7),
+                new Date(2019, 8), new Date(2019, 9), new Date(2019, 10), new Date(2019, 11)
+                ]
+        },
+        vAxis: {
+        viewWindow: {
+        max: 60
+        }
+        }
+        };
+
+        function drawMaterialChart() {
+        var classicChart = new google.visualization.LineChart(chartDiv);
+        classicChart.draw(data, classicOptions);
+        }
+
+        drawMaterialChart();
+
+        }
+    </script>
 </body>
 </html>
